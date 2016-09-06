1521c941da301cd3984bd8058b0beb3ff8152648
Ion Ciobanu <iciobanu@pgs-soft.com>
2016-09-06 16:13:20 +0200

    working project

--- src/main/java/com/pgs/controller/OrdersController.java
+++ src/main/java/com/pgs/controller/OrdersController.java
@@ -1,15 +1,34 @@
 package com.pgs.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.pgs.schema.order.ObjectFactory;
+import com.pgs.schema.order.OrderInquiryResponseType;
+import com.pgs.schema.order.OrderInquiryType;
+import com.pgs.service.orders.Orders;
+
 @Controller("/processOrderPlacement")
 public class OrdersController {
 
+	@Autowired
+	Orders orders;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String processOrderPlacement(ModelMap model) {
+
+		OrderInquiryType orderInquiry = new ObjectFactory().createOrderInquiryType();
+		orderInquiry.setAccountId(365489);
+		orderInquiry.setEan13(5223564163L);
+		orderInquiry.setOrderQuantity(54);
+		orderInquiry.setUniqueOrderId(3);
+
+		OrderInquiryResponseType response = orders.processOrderPlacement(orderInquiry);
+
+		model.addAttribute("orderStatus", response.getOrder().getOrderStatus());
 
 		return "processOrderPlacement";
 	}
---
+++ src/main/java/com/pgs/interceptors/OrderServiceSoapHeaderOutInterceptor.java
@@ -0,0 +1,32 @@
+package com.pgs.interceptors;
+
+import javax.xml.namespace.QName;
+
+import org.apache.cxf.binding.soap.SoapHeader;
+import org.apache.cxf.binding.soap.SoapMessage;
+import org.apache.cxf.binding.soap.interceptor.AbstractSoapInterceptor;
+import org.apache.cxf.binding.soap.interceptor.SoapOutInterceptor;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.jaxb.JAXBDataBinding;
+import org.apache.cxf.phase.Phase;
+
+public class OrderServiceSoapHeaderOutInterceptor extends AbstractSoapInterceptor {
+	public OrderServiceSoapHeaderOutInterceptor() {
+		super(Phase.WRITE);
+		addBefore(SoapOutInterceptor.class.getName());
+	}
+
+	@Override
+	public void handleMessage(SoapMessage message) throws Fault {
+		QName qname = new QName("http://www.pluralsight.com/service/Orders/", "apikey");
+
+		String apikey = "a1b2c3d4e5";
+		try {
+			SoapHeader header = new SoapHeader(qname, apikey, new JAXBDataBinding(new Class[] { apikey.getClass() }));
+
+			message.getHeaders().add(header);
+		} catch (Exception e) {
+			throw new Fault(e);
+		}
+	}
+}
